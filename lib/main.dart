015fdf5acc66427a07f5cb16be8f0e5e0b3ef028
maramrmilad7 <maro12787@gmail.com>
2026-09-04 21:42:26 +0200

    create splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:demoo/screens/homesreen.dart';
+import 'package:demoo/screens/splashScreen.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(CoffeeApp());
 }
+
 class CoffeeApp extends StatelessWidget {
   const CoffeeApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      home: HomeScreen(),
+    return MaterialApp(
+      home: Splash(),
+      title: 'McDonald',
       debugShowCheckedModeBanner: false,
     );
   }
---
+++ lib/screens/splashScreen.dart
@@ -0,0 +1,36 @@
+import 'package:demoo/screens/homesreen.dart';
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: 1), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (c) => HomeScreen()),
+      );
+    });
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xfffeb60d),
+      body: Column(
+        children: [
+          SizedBox(height: 50),
+          Image.asset("assets/logo/logo.png", width: 420),
+        ],
+      ),
+    );
+  }
+}
