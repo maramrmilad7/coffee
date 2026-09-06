540c0261df3553499e1285a4d0c0910984c8e9fa
maramrmilad7 <maro12787@gmail.com>
2026-09-06 19:45:32 +0200

    adding some animation to the coffee menue

---
+++ lib/screens/drinkkdetails.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DrinkDetails extends StatefulWidget {
+  const DrinkDetails({super.key});
+
+  @override
+  State<DrinkDetails> createState() => _DrinkDetailsState();
+}
+
+class _DrinkDetailsState extends State<DrinkDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/screens/homesreen.dart
+++ lib/screens/homesreen.dart
@@ -1,4 +1,5 @@
 import 'package:demoo/models/drinkmodel.dart';
+import 'package:demoo/screens/drinkkdetails.dart';
 import 'package:demoo/widgets/drink.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +11,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  ScrollController controller = ScrollController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,13 +38,33 @@ class _HomeScreenState extends State<HomeScreen> {
 
             Expanded(
               child: ListView.builder(
+                controller: controller,
                 itemCount: DrinkModel.drinks.length,
                 itemBuilder: (context, index) {
                   final drink = DrinkModel.drinks[index];
-                  return DrinkItem(
-                    drinkImage: drink.image,
-                    title: drink.title,
-                    subTitle: drink.subTitle,
+                  return AnimatedBuilder(
+                    animation: controller,
+                    builder: (context, child) {
+                      double offset = 0;
+                      if (controller.hasClients) {
+                        offset = controller.offset / 100 - index;
+                      }
+                      offset = offset.clamp(0, 1);
+                      return Transform.scale(
+                        scale: 1 - (offset * 0.2),
+                        child: GestureDetector(
+                          onTap: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (c) => DrinkDetails()),
+                          ),
+                          child: DrinkItem(
+                            drinkImage: drink.image,
+                            title: drink.title,
+                            subTitle: drink.subTitle,
+                          ),
+                        ),
+                      );
+                    },
                   );
                 },
               ),
